84e1a75a5b180819c2a7a572a7b35eb0a6a3361e
Shehan Jayasooriya <116498102+ShehanJay00@users.noreply.github.com>
2025-09-17 00:57:48 +0530

    Login Page

--- Client/src/components/Header.jsx
+++ Client/src/components/Header.jsx
@@ -3,9 +3,26 @@ import { assets } from '../assets/assets';
 
     const Header = () => {
     return (
-        <div>
+        <div className='flex flex-col items-center justify-center mt-20 px-4 text-center text-gray-800'>
+
             <img src={assets.header_img} alt="Header Img" className="w-36 h-36 rounded-full mb-6"/>
-            <h1>Hey Developer <img className='w-8 aspect-square' src={assets.hand_wave} alt="Hand Wave" /></h1>
+            <h1 className='flex items-center gap-2 text-xl sm:text-3xl font-medium mb-2'>
+                Hey Developer 
+                <img className='w-8 aspect-square' src={assets.hand_wave} alt="Hand Wave" />
+            </h1>
+        
+            <h2 className='text-3xl sm:text-5xl font-semibold mb-4'>
+                Welcome to our app
+            </h2>
+
+            <p className='mb-8 max-w-md'>
+                This is a modern authentication app built with the MERN stack. It features user registration, login, email verification, and password reset functionalities.
+            </p>
+
+            <button className='border border-gray-500 rounded-full px-8 py-2.5 hover:bg-gray-200 transition-all'>
+                Get Started
+            </button>
+
         </div>
     )
     }
--- Client/src/components/Navbar.jsx
+++ Client/src/components/Navbar.jsx
@@ -1,13 +1,18 @@
-    import React from 'react'
     import {assets} from '../assets/assets.js'
+    import { useNavigate } from 'react-router-dom'
 
     const Navbar = () => {
+
+        const navigate = useNavigate();
+
     return (
         <div className='w-full flex py-6 justify-between items-center p-4 sm:p-6 sm:px-24 absolute top-0'>
             <img src={assets.logo} alt="logo" className='w-28 sm:w-32' /> 
 
-            <button className='flex items-center gap-2 border border-grey-500 rounded-full px-6 py-2 text-gray-800 hover:bg-grey-100'>
-                Login <img src={assets.arrow_icon} alt="Arrow Icon" /></button>
+            <button onClick={() => navigate('/login')}
+                className='flex items-center gap-2 border border-grey-500 rounded-full px-6 py-2 text-gray-800 hover:bg-gray-200 transition-all'>
+                Login <img src={assets.arrow_icon} alt="Arrow Icon" />
+            </button>
 
         </div>
     )
